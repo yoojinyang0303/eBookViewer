575c7237d58335ba76c349d7e7d0fdce6acbb148
[SW5] 양유진 <yoojinyang0303@gmail.com>
2023-07-31 21:41:36 +0900

    feat: 상단 책 정보탭 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import "./App.css";
 import epubFile from "./pg11.epub";
 import EPubViewer from "./EpubViewer";
 
 function App() {
   return (
     <div className="App">
-      <header>Epub.js 설치 테스트...!</header>
       <EPubViewer url={epubFile} className="EpubViewer" />
     </div>
   );
