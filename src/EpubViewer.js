75062436bff32ae47e07d609ae7cb701b73303cf
[SW5] 양유진 <yoojinyang0303@gmail.com>
2023-08-09 17:03:30 +0900

    feat: highlight 직접 스타일 적용 및 IndexedDB에 highlight 정보 저장

--- src/EpubViewer.js
+++ src/EpubViewer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Epub from "epubjs";
+import Dexie from "dexie";
 
 const EPubViewer = ({ url }) => {
   const [rendition, setRendition] = useState(null);
@@ -21,7 +22,7 @@ const EPubViewer = ({ url }) => {
         const newRendition = epub.renderTo("area", {
           flow: "paginated",
           width: "100%",
-          height: "92vh",
+          height: "90vh",
         });
 
         // default - 기본 테마
@@ -32,20 +33,32 @@ const EPubViewer = ({ url }) => {
           a: { color: "black !important" },
         });
 
-        // highlighted - 텍스트 드래그 시 적용할 테마
-        newRendition.themes.register("highlighted", {
-          h2: { backgroundColor: "yellow !important" },
-          p: { backgroundColor: "yellow !important" },
-          div: { backgroundColor: "yellow !important" },
-          a: { backgroundColor: "yellow !important" },
-        });
-
         newRendition.themes.select("default");
 
-        newRendition.on("selected", (cfiRange) => {
+        // highlight 적용 + IndexedDB에 하이라이트 정보 저장
+        newRendition.on("selected", async (cfiRange) => {
           if (cfiRange) {
             // 드래그한 텍스트에 하이라이트 적용
-            newRendition.annotations.highlight(cfiRange, {}, (e) => {});
+            const range = newRendition.getRange(cfiRange);
+            // 'div'로 적용하는 경우도 있던데, 그렇게되면 드래그한 텍스트+block요소인 div로 텍스트 파일의 의도치않은 줄바꿈 현상 등이 나타나는 에러 발생
+            // 'div'가 아닌, inline-level 요소인 span 태그를 사용하여 사용자가 딱 드래그한 만큼만 변경값 적용하기.
+            const highlight = document.createElement("span");
+            highlight.style.backgroundColor = "rgba(253,251,165,0.5)";
+            highlight.style.color = "black";
+            highlight.style.position = "float";
+            range.surroundContents(highlight); // Range 객체로 텍스트에 하이라이트 추가
+
+            const text = range.toString();
+            const db = new Dexie("highlightsDB");
+            db.version(1).stores({
+              highlights: "++id, cfiRange, text",
+            });
+
+            try {
+              await db.highlights.add({ cfiRange, text });
+            } catch (error) {
+              console.error("Error adding highlight to IndexedDB: ", error);
+            }
           }
         });
 
@@ -88,7 +101,7 @@ const EPubViewer = ({ url }) => {
       <div
         id="bookInfo"
         style={{
-          height: "8vh",
+          height: "10vh",
           textWeight: "bold",
           textAlign: "center",
           backgroundColor: "rgba(205,205,206,0.3)",
@@ -104,14 +117,14 @@ const EPubViewer = ({ url }) => {
           {bookTitle}
         </h1>
         <div style={{ padding: "1vh 0 1vh 0", fontSize: "1.5rem" }}>
-          page: {currentPage}
+          * page: {currentPage}
         </div>
       </div>
       <div id="ebookContainer" style={{ display: "flex" }}>
         <button onClick={prevPage} style={{ width: "2%" }}>
           &lt;
         </button>
-        <div id="area" style={{ width: "96%" }}></div>
+        <div id="area" style={{ width: "96%", overflow: "hidden" }}></div>
         <button onClick={nextPage} style={{ width: "2%" }}>
           &gt;
         </button>
